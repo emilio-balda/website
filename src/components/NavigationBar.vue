7763d0acfc88685dc8fb9f5ead48601219669a11
Emilio Balda <emilio.balda@aixbrain.de>
2022-03-10 22:06:59 +0100

    refactor: split navigation bar into child components

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,72 +1,14 @@
 <script setup lang="ts">
-import { useStore } from 'vuex'
-
-const store = useStore()
+import NavigationLink from '@/components/NavigationLink.vue'
 </script>
-
 
 <template>
   <div id="nav" class="py-12">
     <p class="text-3xl">
-      <router-link
-        to="/"
-        class="router-link"
-        :class="{ 'router-link-selected': store.state.currentPage === 'Home' }"
-        @click.prevent="store.state.currentPage = 'Home'"
-      >
-        Home
-      </router-link>
-      <router-link
-        to="/services"
-        class="router-link"
-        :class="{ 'router-link-selected': store.state.currentPage === 'Services' }"
-        @click.prevent="store.state.currentPage = 'Services'"
-      >
-        Services
-      </router-link>
-      <router-link
-        to="/portfolio"
-        class="router-link"
-        :class="{ 'router-link-selected': store.state.currentPage === 'Portfolio' }"
-        @click.prevent="store.state.currentPage = 'Portfolio'"
-      >
-        Portfolio
-      </router-link>
-      <router-link
-        to="/testimonials"
-        class="router-link"
-        :class="{ 'router-link-selected': store.state.currentPage === 'Testimonials' }"
-        @click.prevent="store.state.currentPage = 'Testimonials'"
-      >
-        Testimonials
-      </router-link>
-      <router-link
-        to="/references"
-        class="router-link"
-        :class="{ 'router-link-selected': store.state.currentPage === 'References' }"
-        @click.prevent="store.state.currentPage = 'References'"
-      >
-        References
-      </router-link>
-      <router-link
-        to="/contact"
-        class="router-link"
-        :class="{ 'router-link-selected': store.state.currentPage === 'Contact' }"
-        @click.prevent="store.state.currentPage = 'Contact'"
-      >
-        Contact
-      </router-link>
+      <navigation-link to="/" page-name="Home" />
+      <navigation-link to="/services" page-name="Services" />
+      <navigation-link to="/portfolio" page-name="Portfolio" />
+      <navigation-link to="/contact" page-name="Contact" />
     </p>
   </div>
 </template>
-
-<style scoped>
-.router-link {
-  padding: 5px 20px 5px 20px;
-}
-
-.router-link-selected {
-  border-bottom: 8px solid mediumspringgreen;
-  border-image: linear-gradient(to right, darkorchid, royalblue, mediumspringgreen) 1;
-}
-</style>
---
+++ src/components/NavigationLink.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { useStore } from 'vuex'
+
+const store = useStore()
+
+const props = defineProps({
+  pageName: String,
+  to: String,
+})
+</script>
+
+<template>
+  <router-link
+    :to="to"
+    class="router-link"
+    :class="{ 'router-link-selected': store.state.currentPage === pageName }"
+    @click.prevent="store.state.currentPage = pageName"
+  >
+    {{ pageName }}
+  </router-link>
+</template>
+
+<style scoped>
+.router-link {
+  padding: 5px 20px 5px 20px;
+}
+
+.router-link-selected {
+  border-bottom: 8px solid mediumspringgreen;
+  border-image: linear-gradient(to right, darkorchid, royalblue, mediumspringgreen) 1;
+}
+</style>
